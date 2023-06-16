fd6292e05bebdaa6ba3d6ca0db9c7d4948562854
Cheolyong-Kim <cjfdyd9068@pukyong.ac.kr>
2023-06-16 14:40:03 +0900

    add build, deploy code

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/movie/:id" element={<Detail></Detail>}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home></Home>}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail></Detail>}></Route>
       </Routes>
     </Router>
   )
